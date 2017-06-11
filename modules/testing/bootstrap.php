41150e8366441378dc7c414ba8d357e774c72a58
Maxim Zasorin <maximzasorin@gmail.com>
2017-06-12 01:11:18 +0300

    fix chmod constants

--- modules/testing/bootstrap.php
+++ modules/testing/bootstrap.php
@@ -13,11 +13,11 @@ class Testing_Bootstrap
 	static public function defineConstants()
 	{
 		defined('HOSTCMS') || define('HOSTCMS', TRUE);
-		defined('CHMOD') || define('CHMOD', 664);
+		defined('CHMOD') || define('CHMOD', 0755);
+		defined('CHMOD_FILE') || define('CHMOD_FILE', 0664);
 
-		if (!defined('CMS_FOLDER'))
-		{
-			$reflector = new ReflectionClass('Core');
+		if (!defined('CMS_FOLDER')) {
+	    	$reflector = new ReflectionClass('Core');
 			$cmsFolder = realpath(dirname($reflector->getFileName()) . '/../../') . '/';
 
 			define('CMS_FOLDER', $cmsFolder);
